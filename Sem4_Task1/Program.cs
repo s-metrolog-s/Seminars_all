7a3000f5ae4a988e1101abf372946d365d3f8a12
Sergey <metrolog_omsk@mail.ru>
2022-06-18 12:53:52 +0500

    Решили одну задачу

--- Sem4_Task1/Program.cs
+++ Sem4_Task1/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// ДЗ
+
+string number1 = "4653";
+Zadacha13(number1);
+string number2 = "3458";
+Zadacha13(number2);
+string number3 = "38634829";
+Zadacha13(number3);
+
+void Zadacha13(string currentNumber)
+{
+    if (currentNumber.Length < 3)
+    {
+        Console.WriteLine("Третьей цифры нет");
+    }
+    else
+    {
+        Console.WriteLine("Третья цифра: " + currentNumber[2]);
+    }
+}
