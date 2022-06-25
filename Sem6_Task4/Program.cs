90c145bffba2927d5d6eb0c42942c4ee8ed6bfbb
Sergey <metrolog_omsk@mail.ru>
2022-06-25 14:15:26 +0500

    Решили задачи № 4 и 5 семианара 6

---
+++ Sem6_Task4/Program.cs
@@ -0,0 +1,14 @@
+﻿// Вывести числа Фибоначчи, не используя рекурсию
+
+double first = 0;
+double second = 1;
+double finish = 80;
+Console.WriteLine("1. 0");
+Console.WriteLine("2. 1");
+for (int i = 3; i <= finish; i++)
+{
+    double nextNumber = first + second;
+    Console.WriteLine($"{i}. {nextNumber}");
+    first = second;
+    second = nextNumber;
+}
---
+++ Sem6_Task5/Program.cs
@@ -0,0 +1,14 @@
+﻿// Копия заданного массива с помощью поэлементного копирования
+
+int[] A = new int[8] { 5, 8, 9, 10, -5, 0, -3, 1 };
+int[] B = new int[A.Length];
+
+for (int i = 0; i < A.Length; i++)
+{
+    B[i] = A[i];
+}
+
+for (int i = 0; i < A.Length; i++)
+{
+    Console.Write($"{B[i]} \t");
+}
