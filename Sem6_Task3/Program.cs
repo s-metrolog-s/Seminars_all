13dffda7ef651dac6ea52917dfdec5f83a1af39f
Sergey <metrolog_omsk@mail.ru>
2022-06-25 13:58:34 +0500

    Решили задачу № 3 семинара 6

---
+++ Sem6_Task3/Program.cs
@@ -0,0 +1,20 @@
+﻿// Перевод десятичного числа в двоичное
+// Решаем через string и конкотинацию
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+TenToTwo(number);
+
+void TenToTwo(int currentNumber)
+{
+    string result = "";
+    int startNumber = currentNumber;
+    while (currentNumber > 0)
+    {
+        result = currentNumber % 2 + result; //Конкотинация строк
+        currentNumber = currentNumber / 2;
+    }
+    Console.WriteLine($"{startNumber} -> {result}");
+
+}
