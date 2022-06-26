39985c262c3aa8bf6b80f09f35dcbf1eaf1987da
Sergey <metrolog_omsk@mail.ru>
2022-06-26 12:57:12 +0500

    Решили задачу 3 и 4

---
+++ Sem7_Task3/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задача 49: Задайте двумерный массив. 
+// Найдите элементы, у которых оба индекса чётные, 
+// и замените эти элементы на их квадраты.
+
+Random rand = new Random();
+int rows = rand.Next(2, 10);
+int columns = rand.Next(2, 10);
+
+int[,] array = new int[rows,columns];
+
+FillArray(array);
+PrintArray(array);
+FindEvenNumber(array);
+PrintArray(array);
+
+void FillArray(int[,] currentArray)
+{
+    for (int i = 0; i < currentArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < currentArray.GetLength(1); j++)
+        {
+            currentArray[i, j] = rand.Next(1, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] currentArray)
+{
+    for (int i = 0; i < currentArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < currentArray.GetLength(1); j++)
+        {
+            Console.Write(currentArray[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FindEvenNumber(int[,] currentArray)
+{
+    for (int i = 0; i < currentArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < currentArray.GetLength(1); j++)
+        {
+            if (i % 2 == 0 && j % 2 == 0)
+            {
+                currentArray[i, j] += currentArray[i, j];
+            }
+        }
+        Console.WriteLine();
+    }
+}
+
---
+++ Sem7_Task4/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задача 51: Задайте двумерный массив. Найдите сумму 
+// элементов, находящихся на главной диагонали (с индексами (0,0); (1;1) и т.д.
+
+Random rand = new Random();
+int rows = rand.Next(2, 10);
+int columns = rand.Next(2, 10);
+
+int[,] array = new int[rows,columns];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine($"Сумма элементов главной диагонали равна: {SumMainLine(array)}");
+
+
+void FillArray(int[,] currentArray)
+{
+    for (int i = 0; i < currentArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < currentArray.GetLength(1); j++)
+        {
+            currentArray[i, j] = rand.Next(1, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] currentArray)
+{
+    for (int i = 0; i < currentArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < currentArray.GetLength(1); j++)
+        {
+            Console.Write(currentArray[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int SumMainLine(int[,] currentArray)
+{
+    int sum = 0;
+    for (int i = 0; i < currentArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < currentArray.GetLength(1); j++)
+        {
+            if (i == j)
+            {
+                sum = sum + currentArray[i, j];
+            }
+        }
+    }
+    return sum;
+}
