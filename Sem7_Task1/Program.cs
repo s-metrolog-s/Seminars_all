b5a50b741207287079028a0de467732952180270
Sergey <metrolog_omsk@mail.ru>
2022-06-26 12:34:17 +0500

    Решили 1 и 2 задачи

---
+++ Sem7_Task1/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задать двумерный массив, заполенный случайными числами.
+
+Random rand = new Random();
+int rows = rand.Next(2, 10);
+int columns = rand.Next(2, 10);
+
+int[,] array = new int[rows,columns];
+
+Console.WriteLine("Количество строк" + " " + array.GetLength(0));
+Console.WriteLine("Количество столбцов" + " " + array.GetLength(1));
+
+FillArray(array);
+PrintArray(array);
+
+void FillArray(int[,] currentArray)
+{
+    for (int i = 0; i < currentArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < currentArray.GetLength(1); j++)
+        {
+            currentArray[i, j] = rand.Next(-50, 51);
+        }
+    }
+}
+
+void PrintArray(int[,] currentArray)
+{
+    for (int i = 0; i < currentArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < currentArray.GetLength(1); j++)
+        {
+            Console.Write(currentArray[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+
---
+++ Sem7_Task2/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 48: Задайте двумерный массив размера m на n, 
+// каждый элемент в массиве находится по формуле: 
+// A = m+n. Выведите полученный массив на экран.
+
+Random rand = new Random();
+int rows = rand.Next(2, 10);
+int columns = rand.Next(2, 10);
+
+int[,] array = new int[rows,columns];
+
+FillArray(array);
+PrintArray(array);
+
+void FillArray(int[,] currentArray)
+{
+    for (int i = 0; i < currentArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < currentArray.GetLength(1); j++)
+        {
+            currentArray[i, j] = i + j;
+        }
+    }
+}
+
+void PrintArray(int[,] currentArray)
+{
+    for (int i = 0; i < currentArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < currentArray.GetLength(1); j++)
+        {
+            Console.Write(currentArray[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
