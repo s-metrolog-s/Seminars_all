0c667373fac0dbb2a680ffffea354e493a94be16
Sergey <metrolog_omsk@mail.ru>
2022-07-03 13:27:42 +0500

    Задача 9.4 Рекурсия

---
+++ Sem9_Task4/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 67: Напишите программу, которая будет 
+// принимать на вход число и возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+Random rand = new Random();
+int number = 1829546777;
+
+Console.WriteLine("Сумма цифр числа равна " + Recursion(number));
+
+int Recursion(int currentNumber, int sum = 0)
+{
+    if (currentNumber == 0)
+    {
+        //Console.WriteLine("Сумма цифр числа равна " + sum);
+        return sum;
+    }
+    sum += currentNumber % 10;
+    return Recursion(currentNumber/10, sum);
+}
