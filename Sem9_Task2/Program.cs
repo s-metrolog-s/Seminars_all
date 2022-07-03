3dee8845994123db644c7ae63c68e1ea5b30850b
Sergey <metrolog_omsk@mail.ru>
2022-07-03 13:01:18 +0500

    Задача 9.2 Рекурсия

---
+++ Sem9_Task2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 63. Вывести все натуральные числа от 1 до N
+// Используем рекурсиию
+
+int number = 8; // 8 7 6 5 4 3 2 1
+
+Recursion(number);
+
+void Recursion(int currentNumber, int count = 1)
+{
+    if (count > currentNumber) return;
+    Console.WriteLine(count);
+    Recursion(currentNumber, count + 1);
+}
