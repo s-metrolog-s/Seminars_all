9ea98e966baa4e5d0d5125f52f8cd2a40dfea9ce
Sergey <metrolog_omsk@mail.ru>
2022-07-09 13:45:29 +0500

    Рекурсия Фибоначчи

---
+++ Sem10_Task2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 70: Напишите программу, которая на вход 
+// принимает два числа и выдаёт первые N чисел, для которых 
+// каждое следующее равно сумме двух предыдущих.
+
+Random rand = new Random();
+int firstNumber = rand.Next(1, 10);
+int secondNumber = firstNumber + rand.Next(1, 10);
+int count = rand.Next(1, 10);
+
+Console.WriteLine($"Вывести первые {count} чисел для элементов {firstNumber} и {secondNumber}");
+Recursion(firstNumber, secondNumber, count);
+
+void Recursion(int startNumber, int finishNumber, int count)
+{
+    if (count == 0) return;
+    Console.Write(startNumber + "\t");
+    finishNumber = startNumber + finishNumber;
+    startNumber = finishNumber - startNumber;
+
+    Recursion(startNumber, finishNumber, count - 1);
+
+    // Кортеж
+    //(startNumber, finishNumber) = (finishNumber, startNumber + finishNumber);
+}
