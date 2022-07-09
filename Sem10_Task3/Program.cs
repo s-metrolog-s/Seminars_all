cfbdadbac3817d8b61b0dd96d90278a5df8c4dfb
Sergey <metrolog_omsk@mail.ru>
2022-07-09 14:00:59 +0500

    Рекурсия комбинация букв

---
+++ Sem10_Task3/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 71: В некотором машинном алфавите имеются четыре буквы «а», «и», «с» и «в». 
+// Покажите все слова, состоящие из n букв, которые можно построить из букв этого алфавита.
+// n = 2 -> аа, ии, сс, вв, аи, иа, ис, си, ас, са, 
+// ав, ва, ви, ив, св, вс
+
+string alphabet = "аисв";
+int count = 3;
+
+Recursion(alphabet, count);
+
+void Recursion(string alphebet, int count, string result = "")
+{
+    if (count == 0)
+    {
+        Console.WriteLine(result);
+        return;
+    }
+
+    for (int i = 0; i < alphebet.Length; i++)
+    {
+        Recursion(alphabet, count - 1, result + alphabet[i]);
+    }
+}
